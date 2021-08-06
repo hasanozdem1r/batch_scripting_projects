619821787dd6ab32a18d44f119d32e51f76ff899
Hasan Özdemir <34897251+hasanozdem1r@users.noreply.github.com>
2021-08-06 19:43:38 +0300

    Create setup.bat

---
+++ automate-open-tabs/setup.bat
@@ -0,0 +1,9 @@
+@echo off
+rem This script will redirect for your daily websites.
+start "" https://www.google.com/
+start "" https://www.youtube.com/
+start "" https://linkedin.com/
+start "" https://medium.com/
+start "" https://github.com/
+start "" https://stackoverflow.com/
+exit
